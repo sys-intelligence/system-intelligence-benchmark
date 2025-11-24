92ceddc3b8aa223e0094e56d0b6521dcbb4bd9b3
Bogdan-Alexandru Stoica <b.al.stoica@gmail.com>
2025-11-24 15:17:17 -0600

    feature: add updated Docker image and environment bootstrapt scripts

---
+++ benchmarks/arteval_bench/install.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -e  # Exit immediately on error.
+
+# if .venv does not exist, create it
+if [ -d ".venv" ]; then
+    echo "==> .venv already exists, skipping creation."
+else
+    echo "==> Creating .venv directory..."
+
+    python3 -m venv .venv
+    source .venv/bin/activate
+    pip install -r requirements.txt
+    pip install pytest
+    pip install pytest-cov
+    deactivate
+fi
+
+echo "==> ArtEvalBench environment is set up successfully."
