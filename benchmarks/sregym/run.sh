e7e6b4cbfdd246e820dc3547b0e5912e75670c84
Jackcuii <forever_1210@yeah.net>
2025-12-02 02:47:32 -0600

    ensure compatibility with sregym_core

--- benchmarks/sregym/run.sh
+++ benchmarks/sregym/run.sh
@@ -9,7 +9,6 @@ if [ $# -ne 1 ]; then
 fi
 
 MODEL_NAME="$1"
-NEW_MODEL_NAME="${MODEL_NAME//\//_}"
 
 # Note: set it to "openai" if you are using your own model server (vllm)
 # Otherwise, set it to "azure" if you are using azure gpt endpoint
@@ -18,12 +17,14 @@ NEW_MODEL_NAME="${MODEL_NAME//\//_}"
 # export OPENAI_BASE_URL="http://localhost:2327/v1"
 # export OPENAI_API_KEY="EMPTY"
 
-source .venv/bin/activate
-echo "==> Start to run ExampleBench"
+source benchmarks/sregym/sregym_core/.venv/bin/activate
+
+echo "==> Start to run SREGym"
 # Note that if you benchmark has multiple tasks, you need to add --task <task> 
 # in your code to enable task selection.
+
 python src/main.py \
-    --model_name "${MODEL_NAME}"
-    # --save_path "./outputs/examplebench__${NEW_MODEL_NAME}__$(date +"%Y-%m-%d_%H-%M-%S")" \
+    --agent "stratus" \
+    --model_name "${MODEL_NAME}" 
     
 deactivate
