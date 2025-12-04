4b135b76a285aa4536042f0b505c30a2847cda04
Jackcuii <forever_1210@yeah.net>
2025-12-04 02:18:46 -0600

    Small fixes

--- benchmarks/sregym/run.sh
+++ benchmarks/sregym/run.sh
@@ -4,25 +4,15 @@ set -e  # Exit immediately on error.
 
 if [ $# -ne 1 ]; then
     echo "Usage: $0 <model_location>"
-    echo "Example: $0 Qwen/Qwen2.5-7B-Instruct"
+    echo "Example: $0 \"gemini/gemini-2.5-flash\""
     exit 1
 fi
 
 MODEL_NAME="$1"
 
-# Note: set it to "openai" if you are using your own model server (vllm)
-# Otherwise, set it to "azure" if you are using azure gpt endpoint
-# Run self-serving model
-# export OPENAI_API_TYPE="openai"  
-# export OPENAI_BASE_URL="http://localhost:2327/v1"
-# export OPENAI_API_KEY="EMPTY"
-
 source sregym_core/.venv/bin/activate
 
 echo "==> Start to run SREGym"
-# Note that if you benchmark has multiple tasks, you need to add --task <task> 
-# in your code to enable task selection.
-
 python src/main.py \
     --agent "stratus" \
     --model_name "${MODEL_NAME}" 
