0c7769b2f9adf8aed23889f6a3486689f064d6f5
Tarek Elsayed <tareknaser360@gmail.com>
2026-01-27 08:29:04 -0800

    feat(courselab): specify git commit when cloning

--- benchmarks/courselab_bench/data/mit_6_5840_2024/task_3b_raft/sol.sh
+++ benchmarks/courselab_bench/data/mit_6_5840_2024/task_3b_raft/sol.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 set -e
 
-git clone --depth 1 https://github.com/GaryHo34/MIT-6.5840-distributed-systems-labs.git /tmp/mit-solution
+git clone https://github.com/GaryHo34/MIT-6.5840-distributed-systems-labs.git /tmp/mit-solution
+git -C /tmp/mit-solution checkout 95318f79101581ec37d04471e7c4d50d9cb2db3a
 
 cp /tmp/mit-solution/src/raft/append_entries.go src/raft/append_entries.go
 cp /tmp/mit-solution/src/raft/election.go src/raft/election.go
