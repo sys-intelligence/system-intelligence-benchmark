64e2ef1d1cbe8257c1ed7dae8ba5d51086cddf63
Acture <acturea@gmail.com>
2026-03-06 12:14:23 +0800

    chore: remove local proxy helper

--- scripts/use_local_proxy.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-# Source this file to enable local proxy for current shell session.
-export HTTP_PROXY="http://127.0.0.1:7890"
-export HTTPS_PROXY="http://127.0.0.1:7890"
-export ALL_PROXY="socks5://127.0.0.1:7890"
-export http_proxy="$HTTP_PROXY"
-export https_proxy="$HTTPS_PROXY"
-export all_proxy="$ALL_PROXY"
